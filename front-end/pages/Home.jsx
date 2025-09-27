be4e938200ad4dc95330d0f9aee7fa969e21f8c9
josephvutrinh <josephvu.06@gmail.com>
2025-09-27 15:02:09 -0400

    added default background

--- front-end/pages/Home.jsx
+++ front-end/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <>
-        <h1>Welcome to the Home Page</h1>
+            <h1>Welcome to the Home Page</h1>
         </>
     )
 }
