dbe6fe68477a6e7cd02c4abb87f4881e3524e527
Nick Nguyen <nicknguyen@Nicks-MacBook-Pro-2.local>
2025-09-27 17:09:03 -0400

    brand name

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from '/icon.svg'
 import Home from '../pages/Home'
+import Nav from '../components/Nav'
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Home />
+      <Nav />
     </>
   )
 }
